2b13ee4ac897c3cac8b6737959c2177b96a95bb7
Drew Lewis <drewlewis@southalabama.edu>
2017-08-15 13:31:58 -0500

    standard lists with prof names

--- bin/build.bash
+++ bin/build.bash
@@ -16,7 +16,8 @@ pdflatex syllabus-clontz-lecture.tex
 pdflatex syllabus-clontz-tbl.tex
 pdflatex syllabus-lewis-lecture.tex
 pdflatex syllabus-lewis-tbl.tex
-pdflatex standards.tex
+pdflatex standards-lewis.tex
+pdflatex standards-clontz.tex
 pdflatex homework-report.tex
 pdflatex reassessment-form.tex
 pdflatex assessment-calendar-clontz-lecture.tex
@@ -32,8 +33,8 @@ cp syllabus-clontz-lecture.pdf clontz/lecture/
 cp syllabus-clontz-tbl.pdf clontz/tbl/
 cp syllabus-lewis-lecture.pdf lewis/lecture/
 cp syllabus-lewis-tbl.pdf lewis/tbl/
-cp standards.pdf clontz/lecture/
-cp standards.pdf clontz/tbl/
-cp standards.pdf lewis/lecture/
-cp standards.pdf lewis/tbl/
+cp standards-clontz.pdf clontz/lecture/
+cp standards-clontz.pdf clontz/tbl/
+cp standards-lewis.pdf lewis/lecture/
+cp standards-lewis.pdf lewis/tbl/
 # TODO write a Makefile that (might?) handle partial builds more intelligently
