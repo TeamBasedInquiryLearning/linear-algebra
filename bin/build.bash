ae8598b27c3f2132089464411a39f505a8b97dd2
Steven Clontz <steven.clontz@gmail.com>
2017-08-30 10:44:09 -0500

    minimal build script

--- bin/build.bash
+++ bin/build.bash
@@ -2,7 +2,6 @@
 
 #Produce notes and slides
 pdflatex course-slides.tex
-for file in day1-slides-*.tex ; do pdflatex $file ; done
 
 #Produce readiness materials
 ./bin/readiness.bash
@@ -13,10 +12,11 @@ for file in course-notes*.tex ; do pdflatex $file ; done
 #Produce quizzes
 #./bin/quiz.bash
 
-#Produce syllabus and forms
-for file in syllabus-*.tex ; do pdflatex $file ; done
-for file in standards-*.tex ; do pdflatex $file ; done
-for file in homework-report-*.tex ; do pdflatex $file ; done
-for file in reassessment-form-*.tex ; do pdflatex $file ; done
-for file in calendar-*.tex ; do pdflatex $file ; done
-pdflatex homework-problems.tex
+# #Produce syllabus and forms
+# for file in day1-slides-*.tex ; do pdflatex $file ; done
+# for file in syllabus-*.tex ; do pdflatex $file ; done
+# for file in standards-*.tex ; do pdflatex $file ; done
+# for file in homework-report-*.tex ; do pdflatex $file ; done
+# for file in reassessment-form-*.tex ; do pdflatex $file ; done
+# for file in calendar-*.tex ; do pdflatex $file ; done
+# pdflatex homework-problems.tex
