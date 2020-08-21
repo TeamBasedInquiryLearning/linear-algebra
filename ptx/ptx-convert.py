a7d22b826cde5805a177e45de26edf547d6d39ca
Steven Clontz <steven.clontz@gmail.com>
2020-08-21 22:54:48 +0000

    start work on conversion script

--- ptx/ptx-convert.py
+++ ptx/ptx-convert.py
@@ -1,1 +1,18 @@
 import lxml
+import re
+
+# Read in all activities for module
+with open("../tex/modules/1-E/activities.tex") as tex_file:
+    tex_source = tex_file.read()
+
+def command_to_ptx(s,old,new):
+    return re.sub(r"\\"+old+r"{([a-zA-Z \-]*)}",r"<"+new+r">\1</"+new+r">",s)
+
+
+tex_source = tex_source.replace(r"\(",r"<m>")
+tex_source = tex_source.replace(r"\)",r"</m>")
+tex_source = tex_source.replace(r"\[",r"<me>")
+tex_source = tex_source.replace(r"\]",r"</me>")
+tex_source = command_to_ptx(tex_source,"term","term")
+tex_source = command_to_ptx(tex_source,"textbf","em")
+print(tex_source)
