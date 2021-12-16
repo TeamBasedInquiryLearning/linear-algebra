015330e628f12596af1ba43ddbec8c5da5454967
Steven Clontz <steven.clontz@gmail.com>
2021-12-16 22:15:10 +0000

    fix html redirect script

--- redirect-html.py
+++ redirect-html.py
@@ -2,7 +2,7 @@ import os
 
 for filename in os.listdir("docs"):
     if filename.endswith(".html"):
-        html = f'<html><meta http-equiv="refresh" content="0; URL="archive-2021/{filename}" /><body>Redirecting...</body></html>'
+        html = f'<html><head><meta http-equiv="refresh" content="0; URL="archive-2021/{filename}" /></head><body>Redirecting...</body></html>'
         with open(os.path.join("docs",filename),"w") as f:
             f.write(html)
     else:
