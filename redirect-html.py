fb4277bb6a4039113e6a01ee2727c02f519d86a1
TBIL 2021 Fellow <steven.clontz+tbil2021@gmail.com>
2022-07-21 19:48:04 +0000

    more redirect edits

--- redirect-html.py
+++ redirect-html.py
@@ -1,9 +1,9 @@
 import os
 
-for filename in os.listdir("legacy-la"):
+for filename in os.listdir("output/deploy"):
     if filename.endswith(".html"):
-        html = f'<html><head><meta http-equiv="refresh" content="0;url=./legacy/{filename}" /></head><body>Redirecting...</body></html>'
-        with open(os.path.join("legacy-la",filename),"w") as f:
+        html = f'<html><head><meta http-equiv="refresh" content="5;url=./legacy/{filename}" /></head><body><p>Linear Algebra for TBIL has been updated! <a href="./index.html">Learn more.</a></p><p>Redirecting to <a href="./legacy/{filename}">legacy edition</a> of Linear Algebra for TBIL in five seconds...</p></body></html>'
+        with open(os.path.join("output/deploy",filename),"w") as f:
             f.write(html)
     else:
         continue
